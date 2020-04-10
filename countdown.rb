0421633c290dc9c86ffb06b7b2a385d90b912cf6
Elliot Blanchard <public@elliotblanchard.com>
2020-04-10 13:59:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  time_left = number
+  while time_left > 0
+    puts "#{time_left} SECOND(S)!"
+    time_left -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
