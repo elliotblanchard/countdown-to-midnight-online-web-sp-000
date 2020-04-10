e65c6a01beeefd5ee08945be525c6f3a0dda5be6
Elliot Blanchard <public@elliotblanchard.com>
2020-04-10 13:50:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  time_left = number
+  while number < 0
+    puts "#{number} SECOND(S)!"
+    number -= number
+  end
+  "HAPPY NEW YEAR!"
 end
